0af2f5d7a8988732af5260b16dd0e04af8889922
Dragard <viktorteslin@gmail.com>
2019-01-23 14:46:36 +0300

    mybatis generic

--- server/src/main/java/org/dragard/projectmanager/repository/ProjectMyBatisRepositoryImpl.java
+++ server/src/main/java/org/dragard/projectmanager/repository/ProjectMyBatisRepositoryImpl.java
@@ -2,27 +2,16 @@ package org.dragard.projectmanager.repository;
 
 import org.apache.ibatis.session.SqlSession;
 import org.dragard.projectmanager.api.mybatis.mapper.ProjectMapper;
-import org.dragard.projectmanager.api.mybatis.mapper.TaskMapper;
 import org.dragard.projectmanager.api.repository.ProjectRepository;
 import org.dragard.projectmanager.entity.Project;
-import org.dragard.projectmanager.entity.Task;
 
 import java.util.Collection;
 
-public class ProjectMyBatisRepositoryImpl extends AbstractMybatisRepository<Project>
+public class ProjectMyBatisRepositoryImpl extends AbstractJobMybatisRepository<Project>
     implements ProjectRepository {
 
     public ProjectMyBatisRepositoryImpl() {
         super(ProjectMapper.class);
     }
 
-    @Override
-    public Collection<Project> getElementsByUserId(String id) throws Exception {
-        final SqlSession session = getSessionFactory().openSession();
-        final ProjectMapper tm = session.getMapper(ProjectMapper.class);
-        final Collection<Project> result = tm.getByUserId(id);
-        session.commit();
-        session.close();
-        return result;
-    }
 }
